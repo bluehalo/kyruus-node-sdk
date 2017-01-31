d82d7051576a49aca26adda5e3b1b5d796a6ae15
Grant Kingsbury <gkingsbury@asymmetrik.com>
2017-01-31 17:24:18 -0500

    Adding documentations and updating adds/deletes

--- query-builder.js
+++ query-builder.js
@@ -19,46 +19,100 @@ const _ = require('lodash');
 
 class FilterObject {
     constructor(value, type = '') {
-        if (! _.isArray(this.value)) value = [value];
-        this.value = value;
-
-        if (type !== 'or' || type !== '^') this._type = 'and';
-        else this._type = type;
+        if (! _.isArray(this._value)) value = [value];
+        this._value = value;
+        this.setType(type);
     }
-
+    /**
+     * @function getType
+     * @summary Returns the conjugation type (or(|) or and(^))
+     * @return {string}
+     */
     getType() {
         return this._type;
     }
 
-    setType(type) {
-        this._type = type === 'or' ? 'or' : 'and';
+    /**
+     * @function remove
+     * @summary Removes the value from the filter
+     * @param {string} value - value to remove
+     * @return {string}
+     */
+    remove(value) {
+        this.remove(value);
+    }
 
+    /**
+     * @function delete
+     * @summary Removes the value from the filter
+     * @param {string} value - value to remove
+     * @return {string}
+     */
+    delete(value) {
+        _.forEach(value, (val) => {
+            if(val instanceof FilterObject) {
+                val.remove(value);
+            }
+            else {
+                _.pull(this.value )
+            }
+        });
+    }
+
+    /**
+     * @function setType
+     * @summary Sets the conjugation type (or(|) or and(^))
+     * @return {string}
+     */
+    setType(type) {
+        if (type !== '^') type = '|';
+        this._type = type;
         return this;
     }
 
+    /**
+     * @function checkType
+     * @summary Compares the conjugation type (or(|) or and(^))
+     * @return {boolean}
+     */
     checkType(type) {
         return type === this._type;
     }
 
+    /**
+     * @function append
+     * @summary Adds a value to the filter
+     * @return {string}
+     */
     append(value) {
         if (value instanceof FilterObject && value.checkType(this._type)) {
-            this.value = _.union(value.value);
+            this._value = _.union(value._value);
         }
         else {
-            this.value.push(value);
+            this._value.push(value);
         }
 
         return this;
+    }
+    /**
+     * @function toString
+     * @summary Converts the filter object into seperating its filters by its conjugation type
+     * @return {string}
+     */
+    toString() {
+        console.log(this._value);
+        return _.join(this._value, this._type);
     }
 }
 
 
 /**
  * @class k
- * @property {object} _filter - filter object used to build the Kyruus search filter
+ * @property {FilterObject} _filter - filter object used to build the Kyruus search filter
  * @property {KyruusVector} _vector - which vector to use in the Kyruus search if any
  * @property {string} [_location.location] - which location to use in the Kyruus search if any
  * @property {string} [_location.distance] - what distance from a location to use in the Kyruus search if any
+ * @property {object} [_params] - Object of key:value pairs for kyruus api parameters
  */
 class k {
 
@@ -95,6 +149,7 @@ class k {
         // Kyruus only allows for one vector object, this vector object will enforce that
         this._vector = {field: null, value: null};
         this._location = {location: null, distance: null};
+        this._params = [];
 
         this._currentFilter = '';
     }
@@ -123,8 +178,10 @@ class k {
      * @param {string} value - value the vector should be looking for
      * @return {k}
      */
-    _vector(field, value) {
-        this._vector = {field: field, value: value};
+    vector(field, value) {
+        this._vector['field'] = field;
+        this._vector['value'] = value;
+        console.log(this._vector);
         return this;
     }
 
@@ -135,7 +192,7 @@ class k {
      * @return {k}
      */
     name(name) {
-        return this._vector(this.NAME, name);
+        return this.vector(this.NAME, name);
     }
 
     /**
@@ -146,7 +203,7 @@ class k {
      */
     // Figure out of this is correct
     specialtySynonym(synonym) {
-        return this._vector(this.SPECIALTYSYNONYM, synonym);
+        return this.vector(this.SPECIALTYSYNONYM, synonym);
     }
 
     /**
@@ -157,7 +214,7 @@ class k {
      */
     // Figure out of this is correct
     clinicalExperience(experience) {
-        return this._vector(this.CLINICALEXPERIENCE, experience);
+        return this.vector(this.CLINICALEXPERIENCE, experience);
     }
 
     /**
@@ -168,7 +225,7 @@ class k {
      */
     // Figure out of this is correct
     practiceGroup(group) {
-        return this._vector(this.PRACTICEGROUP, group);
+        return this.vector(this.PRACTICEGROUP, group);
     }
 
     /**
@@ -179,7 +236,7 @@ class k {
      */
     // Figure out of this is correct
     unified(unified) {
-        return this._vector(this.UNIFIED, unified);
+        return this.vector(this.UNIFIED, unified);
     }
 
     /*
@@ -194,14 +251,7 @@ class k {
      * @return {k}
      */
     filterOther(field, value, conjunction = 'or') {
-        if (value instanceof k) {
-            value = k.filter;
-        }
-
         if (this._filter[field]) {
-            value = new FilterObject(value);
-
-
             if (this._filter[field].checkType(conjunction)) {
                 this._filter[field].append(value);
             }
@@ -211,83 +261,204 @@ class k {
             }
         }
         else {
-            value = new FilterObject(value);
-            this._filter[field] = value;
+            this._filter[field] = new FilterObject(value);;
         }
 
-        this._currentFilter = value;
+        this._currentFilter = field;
 
         return this;
     }
 
-    npi(...npis, conjunction = 'or') {
+    /**
+     * @function removeFilter
+     * @summary Removes a field from the query
+     * @param {string} field - field to remove
+     * @return {k}
+     */
+    param(field, value) {
+        this._params[field] = value;
+        return this;
+    }
+
+    /**
+     * @function clearVector
+     * @summary Removes a field from the query
+     * @param {string} field - field to remove
+     * @return {k}
+     */
+    clearVector() {
+        this._vector = {field: null, value: null};
+        return this;
+    }
+
+    /**
+     * @function removeFromFilter
+     * @summary Removes a value from a filter
+     * @param {string} field - filter field
+     * @param {string} value - value to remove from filter
+     * @return {k}
+     */
+    removeFromFilter(field, value) {
+        this.deleteFromFilter(field, value);
+    }
+
+    /**
+     * @function deleteFromFilter
+     * @summary Removes a value from a filter
+     * @param {string} field - filter field
+     * @param {string} value - value to remove from filter
+     * @return {k}
+     */
+    deleteFromFilter(field, value) {
+        if (this._filter instanceof FilterObject) {
+            if(this._filter[field]) {
+                this._filter[field].remove(value);
+            }
+        }
+        return this;
+    }
+
+    /**
+     * @function removeFilter
+     * @summary Removes a field from the query
+     * @param {string} field - field to remove
+     * @return {k}
+     */
+    remove(field) {
+        return this.delete(field);
+    }
+
+    /**
+     * @function deleteFilter
+     * @summary Removes a field from the query
+     * @param {string} field - field to remove
+     * @return {k}
+     */
+    delete(field) {
+        delete this._filter[field];
+        delete this._params[field];
+        if (this._vector.field === field) {
+            this.clearVector();
+        }
+
+        return this;
+    }
+
+    /**
+     * @function npis
+     * @summary Adds npis to query
+     * @param {...string} npis - npis to add
+     * @return {k}
+     */
+    npis(...npis) {
         _.forEach(npis, (npi) => {
-            this.filterOther('npi', npi, conjunction);
+            this.filterOther('npi', npi);
         });
         return this;
     }
 
-    filterGender(...genders, conjunction = 'or') {
-        _.forEach(genders, (gender) => {
-            this.filterOther('gender', gender, conjunction);
-        });
-        return this;
+    /**
+     * @function gender
+     * @summary Adds gender to query
+     * @param {string} gender - gender to add
+     * @return {k}
+     */
+    gender(gender) {
+        return this.filterOther('gender', gender);
     }
 
-    filterLocationName(...locations, conjunction = 'or') {
+    /**
+     * @function gender
+     * @summary Adds location names to query
+     * @param {string} gender - gender to add
+     * @return {k}
+     */
+    locationNames(...locations) {
         _.forEach(locations, (location) => {
-            this.filterOther('locations.name', location, conjunction);
+            this.filterOther('locations.name', location);
         });
         return this;
 
     }
 
-    filterSpecialties(...specialties, conjunction = 'or') {
+    /**
+     * @function specialties
+     * @summary Adds specialties to query
+     * @param {...string} specialties - specialties to add
+     * @return {k}
+     */
+    specialties(...specialties) {
         _.forEach(specialties, (specialty) => {
-            this.filterOther('specialties.specialty.untouched', specialty, conjunction);
+            this.filterOther('specialties.specialty.untouched', specialty);
         });
         return this;
     }
 
-    filterSubSpecialties(...specialties, conjunction = 'or') {
+    /**
+     * @function subSpecialties
+     * @summary Adds sub-specialties to query
+     * @param {...string} specialties - sub-specialties to add
+     * @return {k}
+     */
+    subSpecialties(...specialties) {
         _.forEach(specialties, (specialty) => {
-            this.filterOther('specialties.subspecialty.untouched', specialty, conjunction);
+            this.filterOther('specialties.subspecialty.untouched', specialty);
         });
         return this;
     }
 
-    filterPracticeFocus(...focuses, conjunction = 'or') {
-        _.forEach(focuses, (focus) => {
-            this.filterOther('specialties.practice_focus.untouched', focus, conjunction);
-        });
-        return this;
+    /**
+     * @function subSpecialties
+     * @summary Adds sub-specialties to query
+     * @param {...string} specialties - sub-specialties to add
+     * @return {k}
+     */
+    practiceFocus(...focuses) {
+        return this.filterOther('specialties.practice_focus.untouched', focus);
     }
 
-    filterLocationCity(...cities, conjunction = 'or') {
+    /**
+     * @function cityLocations
+     * @summary Adds city locations to query
+     * @param {...string} cities - cities to add
+     * @return {k}
+     */
+    cityLocations(...cities) {
         _.forEach(cities, (city) => {
-            this.filterOther('locations.city', city, conjunction);
+            this.filterOther('locations.city', city);
         });
         return this;
     }
 
-    filterLanguage(...languages, conjunction = 'or') {
+    /**
+     * @function languages
+     * @summary Adds languages to query
+     * @param {...string} cities - languages to add
+     * @return {k}
+     */
+    languages(...languages) {
         _.forEach(languages, (language) => {
-            this.filterOther('languages.language', language, conjunction);
+            this.filterOther('languages.language', language);
         });
         return this;
     }
 
-    filterAcceptingNewPatients(accepts = true) {
+    /**
+     * @function filterAcceptingNewPatients
+     * @summary Adds accepting_new_patients filter to query
+     * @param {boolean} accepts - filter on true/false if the provider is accepting new patients (default true)
+     * @return {k}
+     */
+    acceptingNewPatients(accepts = true) {
         return this.filterOther('accepting_new_patients', accepts);
     }
 
-    and(...values) {
-        _.forEach(values, (value) => {
-            return this.filterOther(this._currentFilter, value, 'and');
-        });
-        return this;
-    }
-
+    /**
+     * @function or
+     * @summary Adds an additional value to the current filter as an or statement
+     * @param {...string} values - Value(s) to append to the current filter key
+     * @return {k}
+     */
     or(...values) {
         _.forEach(values, (value) => {
             this.filterOther(this._currentFilter, value, 'or');
@@ -295,9 +466,17 @@ class k {
         return this;
     }
 
-    andObject(...values) {
-        _.forEach(values, (value) => {
-            this.filterOther(this._currentFilter, value, '^');
+    /**
+     * @function with
+     * @summary Adds additional filters to the current filter as a kyruus and (^) statement. Note: and statements can only be applied to same-object types. See kyruus documentation for more detail
+     * @param {...object} keyValues - key value pairs to add. Key being the additional filter, and value being the string value on what to filter on.
+     * @return {k}
+     */
+    with(...keyValues) {
+        _.forEach(keyValues, (value) => {
+            _.forIn(value, (value, key) => {
+                this.filterOther(this._currentFilter, `${key}:${value}`, '^');
+            });
         });
         return this;
     }
@@ -307,25 +486,68 @@ class k {
      * Search sorting and page selection
      */
 
+     /**
+      * @function shuffle
+      * @summary Adds a seed to shuffle return results
+      * @param {string} seed - String to seed the shuffle with
+      * @return {k}
+      */
     shuffle(seed) {
-        return this.other('shuffle_seed', seed);
+        return this.param('shuffle_seed', seed);
     }
 
+    /**
+     * @function shuffle
+     * @summary Adds a seed to shuffle return results
+     * @param {string} seed - String to seed the shuffle with
+     * @return {k}
+     */
     sort(field) {
-        return this.other('sort', field);
+        return this.param('sort', field);
     }
 
+    /**
+     * @function pageSize
+     * @summary Sets the page size for return results
+     * @param {string} size - Number of results per page
+     * @return {k}
+     */
     pageSize(size) {
-        return this.other('per_page', size);
+        return this.param('per_page', size);
     }
 
+    /**
+     * @function pageNumber
+     * @summary Adds the page number to return results. The results are going to be [pageSize*pageNumber...pageSize*pageNumber+pageNumber-1]
+     * @param {string} number - Page number
+     * @return {k}
+     */
     pageNumber(number) {
-        return this.other('page', number);
+        return this.param('page', number);
     }
 
-    get queryParams() {
+    /**
+     * @function toString
+     * @summary Converts object into a string formatted filter string for kyruus api calls
+     * @return {string}
+     */
+    toString() {
+        let queryParams = []
+        let addQueryParam = function (val, key) {
+            queryParams.push(`${key}=${val}`);
+        }
+        _.map(this._params, addQueryParam);
+        let vectorKey = _.get(this,'_vector[field]', null);
+        let vectorValue = _.get(this,'_vector[value]', null);
+        if(vectorKey && vectorValue) {
+            addQueryParam(vectorValue, vectorKey);
+        }
+        _.forIn(this._filter, (value, key) => {
+            queryParams.push(`filter=${key}:${value}`);
+        });
+
+        return _.size(queryParams) > 0 ? '?'+_.join(queryParams, '&') : '';
 
     }
 }
-
 module.exports = k;
